d814a8fece7d22289c6735610e09048dca74010b
shibbusharma <shivam.gvar22@gmail.com>
2022-12-06 13:02:05 +0530

    Second Commit

--- src/main/java/com/webapp/Webapp/controller/MainController.java
+++ src/main/java/com/webapp/Webapp/controller/MainController.java
@@ -1,15 +1,45 @@
 package com.webapp.Webapp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.webapp.Webapp.model.Employee;
+import com.webapp.Webapp.repository.EmployeeRepository;
+import com.webapp.Webapp.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class MainController {
 
-    @GetMapping("/")
-    public String home(){
-        return "Welcome to GoodVibes Placements";
+    @Autowired
+    private EmployeeService service;
+
+
+    @PostMapping("/save")
+   public Employee saveEmployee(@RequestBody Employee emp){
+
+       return service.saveEmployee(emp);
+   }
+
+   @GetMapping("/all")
+   public List<Employee> getAllEmployee(){
+        return service.getAllEmployee();
+   }
+
+   @GetMapping("/employee/{id}")
+   public Employee findEmpById( @PathVariable("id") int empId ){
+        return service.findEmpById(empId);
     }
 
+    @PutMapping("/update/{id}")
+    public Employee updateEmployee(@RequestBody Employee emp , @PathVariable("id") int empId){
+
+        return service.updateEmployee(empId, emp);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteEmployee(@PathVariable("id") int empId){
+         service.deleteEmployee(empId);
+        return "Employee deleted successfully!!";
+    }
 }
